510ae3277da2fb1e13ab74253e051e8480fe0507
sunzhiyong7 <sunzhiyong@jd.com>
2019-01-22 10:30:33 +0800

    simple compelete

---
+++ index.js
@@ -0,0 +1,16 @@
+const yaml = require('js-yaml');
+const fs = require('fs');
+const path = require('path');
+
+// Get document, or throw exception on error
+
+try {
+  var doc = yaml.safeLoad(fs.readFileSync('./source/index.yml', 'utf8'));
+  // let str = yaml.safeDump(doc);
+  console.log(doc);
+  fs.writeFile('./dist/index.js', JSON.stringify(doc), function (err) {
+    console.log('err', err);
+  })
+} catch (e) {
+  console.log(e);
+}
